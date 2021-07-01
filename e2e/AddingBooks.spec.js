44d752239f1f2faf70f7f02156bc9c145d11352c
Yusril A. P <yusriladr.37@gmail.com>
2021-07-01 21:02:45 +0700

    Add create uncompleted book scenario

--- e2e/AddingBooks.spec.js
+++ e2e/AddingBooks.spec.js
@@ -8,7 +8,7 @@ Before(({ I }) => {
   I.seeElement('#new-book-form-toggler');
 });
 
-Scenario('should be able create a new book', async ({ I }) => {
+Scenario('should be able create a new completed book', async ({ I }) => {
   I.click('#new-book-form-toggler');
 
   const newBook = {
@@ -26,6 +26,39 @@ Scenario('should be able create a new book', async ({ I }) => {
   if (newBook.isComplete) I.checkOption('#new-book-isComplete');
 
   I.click('#new-book-modal button[type=submit]');
+
+  I.seeNumberOfElements('.book-item', 1);
+
+  const showedBook = {
+    title: await I.grabTextFrom('.book-item .book-title'),
+    author: await I.grabTextFrom('.book-item .book-author'),
+    year: await I.grabTextFrom('.book-item .book-year'),
+    isComplete: await I.grabTextFrom('.book-item .toggle-isComplete') === 'Belum selesai',
+  };
+
+  assert.deepStrictEqual(newBook, showedBook);
+});
+
+Scenario('should be able create a new uncompleted book', async ({ I }) => {
+  I.click('#new-book-form-toggler');
+
+  const newBook = {
+    title: 'Test title',
+    author: 'Test author',
+    year: `${new Date().getFullYear() - 1}`,
+    isComplete: false,
+  };
+
+  I.waitForClickable('#new-book-title');
+
+  I.fillField('#new-book-title', newBook.title);
+  I.fillField('#new-book-author', newBook.author);
+  I.fillField('#new-book-year', newBook.year);
+  if (newBook.isComplete) I.checkOption('#new-book-isComplete');
+
+  I.click('#new-book-modal button[type=submit]');
+
+  I.seeInCurrentUrl('/uncompleted-list');
 
   I.seeNumberOfElements('.book-item', 1);
 
